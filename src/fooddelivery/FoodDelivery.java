a3e62d5218f78391250451147dd53419618e9f4f
TangLyheng <tanglyhengmbr@gmai.com>
2025-12-01 10:08:31 +0700

    Lyheng 1/12/2025

--- src/fooddelivery/FoodDelivery.java
+++ src/fooddelivery/FoodDelivery.java
@@ -30,10 +30,10 @@ public class FoodDelivery {
         {
             System.out.println(e.getMessage());
         } 
-        //new FormLogin();
+        new FormLogin();
         //new ForgetPassword();
         //new ResetPassword();
         //new UserForm();
-        new MainForm();
+        //new MainForm();
     }
 }
